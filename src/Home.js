50c93938f7715bb616ae945abb6bb3d109d686f5
babypilot007 <skypirateee@gmail.com>
2024-02-28 20:16:45 -0500

    commit_new

--- src/Home.js
+++ src/Home.js
@@ -27,7 +27,9 @@ function Home() {
               <p>I have acquired around <span>1200 Hours</span> of Total Flight Time.</p>
 <br></br>
               <p>Currently Instructing at : </p><br></br>
-      <button><a href="https://www.p6aviation.com/"><img src={p6} alt="p6"></img></a></button>
+              <div className='img'>
+            <a href="https://www.p6aviation.com/"><img src={p6} alt="p6"></img></a>
+            </div>
 </div>
       
   </div>
