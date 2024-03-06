1b1dac0d4feb59cd4ef676073bd9f956936ea1c6
babypilot007 <skypirateee@gmail.com>
2024-03-06 18:43:19 -0500

    added contact page

--- src/Contact.js
+++ src/Contact.js
@@ -4,6 +4,7 @@ import insta from './images/insta.png'
 import linked from './images/linked.png'
 import whatsapp from './images/whatsapp.png'
 import call from './images/call.png'
+import email from './images/email.png'
 
 
 
@@ -16,10 +17,11 @@ function Contact() {
             <div className='socials'>
         <h1>Contact</h1>
                 <div className='socicons'>
-                     <a href="https://www.instagram.com/curry_wingz"><img src={insta} alt="Instagram"></img></a>
+                        <a href="mailto: cfihimalay@gmail.com"><img src={email} alt="Linked In" height="54px"></img></a>
+                        <br></br><a href=" tel: +1 201-654-5586"><img src={call} alt="Call" height="48px"></img></a>
+                        <br></br> <a href="https://www.instagram.com/curry_wingz"><img src={insta} alt="Instagram"></img></a>
                     <br></br> <a href="https://www.linkedin.com/in/himalay-jariwala-b1524492"><img src={linked} alt="Linked In"></img></a>
                     <br></br> <a href="https://api.whatsapp.com/send?phone=2016545586"><img src={whatsapp} alt="Whatsapp"></img></a>
-                    <br></br><a href=" tel: +1 201-654-5586"><img src={call} alt="Call" height="48px"></img></a>
 
                  </div>
         </div>
