38f2db6311cababb1b64cc70abdaff029541b179
babypilot007 <skypirateee@gmail.com>
2024-03-06 20:26:40 -0500

    css fixes

--- src/Home.js
+++ src/Home.js
@@ -25,7 +25,6 @@ function Home() {
               <p>I am a Certified Flight Instructor,</p>
               <p>With About <span>600 hours of Dual</span> Instructions given.</p>
               <p>I have acquired around <span>1200 Hours</span> of Total Flight Time.</p>
-<br></br>
               <p>Currently Instructing at : </p><br></br>
               
               <div className='img'>
